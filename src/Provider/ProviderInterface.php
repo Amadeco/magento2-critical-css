03d8f7e8c84aba41cc1fd20cd3b2c846b82378a0
Amadeco <2975845+Amadeco@users.noreply.github.com>
2026-02-16 10:10:24 +0100

    Update ProviderInterface.php

--- src/Provider/ProviderInterface.php
+++ src/Provider/ProviderInterface.php
@@ -1,34 +1,56 @@
 <?php
 
+declare(strict_types=1);
+
 namespace M2Boilerplate\CriticalCss\Provider;
 
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\View\LayoutInterface;
 use Magento\Store\Api\Data\StoreInterface;
-use Magento\Store\Model\StoreManagerInterface;
 
+/**
+ * Interface ProviderInterface
+ *
+ * Contract for classes that provide URLs for Critical CSS generation
+ * and identify requests to serve that CSS.
+ */
 interface ProviderInterface
 {
-
     /**
-     * @return string[]
+     * Get a list of URLs to process for the given store.
+     *
+     * @param StoreInterface $store
+     * @return array<string, string> Map of [Identifier => URL]
      */
     public function getUrls(StoreInterface $store): array;
 
     /**
+     * Get the unique name of the provider.
+     *
      * @return string
      */
     public function getName(): string;
 
     /**
+     * Check if the provider is active/available.
+     *
      * @return bool
      */
     public function isAvailable(): bool;
 
     /**
+     * Get the execution priority (higher is earlier).
+     *
      * @return int
      */
     public function getPriority(): int;
 
+    /**
+     * Determine the CSS identifier for a given request.
+     *
+     * @param RequestInterface $request
+     * @param LayoutInterface $layout
+     * @return string|null Returns the identifier if this provider handles the request, otherwise null.
+     */
     public function getCssIdentifierForRequest(RequestInterface $request, LayoutInterface $layout): ?string;
 }
