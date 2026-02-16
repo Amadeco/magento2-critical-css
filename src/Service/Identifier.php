d39a211055508e6c3fedea13058bb2f9fedd2f8b
Amadeco <2975845+Amadeco@users.noreply.github.com>
2026-02-16 10:00:48 +0100

    Update Identifier.php

--- src/Service/Identifier.php
+++ src/Service/Identifier.php
@@ -1,28 +1,48 @@
 <?php
 
+declare(strict_types=1);
+
 namespace M2Boilerplate\CriticalCss\Service;
 
 use M2Boilerplate\CriticalCss\Provider\ProviderInterface;
 use Magento\Framework\Encryption\Encryptor;
 use Magento\Store\Api\Data\StoreInterface;
 
+/**
+ * Generates unique identifiers for Critical CSS files based on context.
+ */
 class Identifier
 {
     /**
-     * @var Encryptor
+     * @param Encryptor $encryptor
      */
-    protected $encryptor;
-
-    public function __construct(Encryptor $encryptor)
-    {
-        $this->encryptor = $encryptor;
+    public function __construct(
+        protected Encryptor $encryptor
+    ) {
     }
 
+    /**
+     * Generate a unique hash for a specific provider, store, and request identifier.
+     *
+     * Format: [STORE_CODE]PROVIDER_IDENTIFIER
+     *
+     * @param ProviderInterface $provider
+     * @param StoreInterface $store
+     * @param string $identifier
+     * @return string
+     */
+    public function generateIdentifier(
+        ProviderInterface $provider,
+        StoreInterface $store,
+        string $identifier
+    ): string {
+        $uniqueIdentifier = sprintf(
+            '[%s]%s_%s',
+            $store->getCode(),
+            $provider->getName(),
+            $identifier
+        );
 
-    public function generateIdentifier(ProviderInterface $provider, StoreInterface $store, $identifier)
-    {
-        $uniqueIdentifier = sprintf('[%s]%s_%s', $store->getCode(), $provider->getName(), $identifier);
         return $this->encryptor->hash($uniqueIdentifier, Encryptor::HASH_VERSION_MD5);
     }
-
 }
