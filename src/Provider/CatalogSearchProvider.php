6d272ba1898537cfe608dd8ff6ff86aecd7ce066
Amadeco <2975845+Amadeco@users.noreply.github.com>
2026-02-16 10:11:50 +0100

    Update CatalogSearchProvider.php

--- src/Provider/CatalogSearchProvider.php
+++ src/Provider/CatalogSearchProvider.php
@@ -1,73 +1,92 @@
 <?php
 
-namespace M2Boilerplate\CriticalCss\Provider;
+declare(strict_types=1);
 
+namespace M2Boilerplate\CriticalCss\Provider;
 
 use Magento\Framework\App\Request\Http;
 use Magento\Framework\App\RequestInterface;
 use Magento\Framework\UrlInterface;
 use Magento\Framework\View\LayoutInterface;
-use Magento\Store\Api\Data\StoreInterface;
+use Magento\Search\Model\Query;
 use Magento\Search\Model\ResourceModel\Query\CollectionFactory;
+use Magento\Store\Api\Data\StoreInterface;
 
+/**
+ * Provider for Search Result pages.
+ */
 class CatalogSearchProvider implements ProviderInterface
 {
-    const NAME = 'catalogsearch';
+    public const NAME = 'catalogsearch';
 
     /**
-     * @var UrlInterface
+     * @param UrlInterface $url
+     * @param CollectionFactory $queryCollectionFactory
      */
-    protected $url;
-    /**
-     * @var CollectionFactory
-     */
-    protected $queryCollectionFactory;
-
-    public function __construct(UrlInterface $url, CollectionFactory $queryCollectionFactory)
-    {
-        $this->url = $url;
-        $this->queryCollectionFactory = $queryCollectionFactory;
+    public function __construct(
+        private readonly UrlInterface $url,
+        private readonly CollectionFactory $queryCollectionFactory
+    ) {
     }
 
-
+    /**
+     * @inheritDoc
+     */
     public function getUrls(StoreInterface $store): array
     {
         $urls = [
             'catalogsearch_advanced_index' => $store->getUrl('catalogsearch/advanced'),
             'search_term_popular' => $store->getUrl('search/term/popular'),
         ];
-        /** @var \Magento\Search\Model\Query $term */
+
+        /** @var Query|null $term */
         $term = $this->queryCollectionFactory
             ->create()
-            ->setPopularQueryFilter($store->getId())
-            ->setPageSize(1)->load()->getFirstItem();
-        if ($term->getQueryText()) {
+            ->setPopularQueryFilter((int)$store->getId())
+            ->setPageSize(1)
+            ->load()
+            ->getFirstItem();
+
+        if ($term && $term->getId() && $term->getQueryText()) {
             $urls['catalogsearch_result_index'] = $store->getUrl(
                 'catalogsearch/result',
                 ['_query' => ['q' => $term->getQueryText()]]
             );
         }
+        
         return $urls;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getName(): string
     {
         return self::NAME;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function isAvailable(): bool
     {
         return true;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getPriority(): int
     {
         return 1300;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getCssIdentifierForRequest(RequestInterface $request, LayoutInterface $layout): ?string
     {
-        if ($request->getModuleName() !== 'catalogsearch' || !$request instanceof Http) {
+        if (!$request instanceof Http || $request->getModuleName() !== 'catalogsearch') {
             return null;
         }
 
@@ -77,12 +96,7 @@ class CatalogSearchProvider implements ProviderInterface
             'catalogsearch_result_index',
             'search_term_popular',
         ];
-        if (in_array($actionName, $supportedActions)) {
-            return $actionName;
-        }
 
-        return null;
+        return in_array($actionName, $supportedActions, true) ? $actionName : null;
     }
-
-
 }
